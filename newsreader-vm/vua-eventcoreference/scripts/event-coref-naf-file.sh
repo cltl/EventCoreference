8eb39a123646e7094be0db305924b0eb37af12cf
piekvossen <piek.vossen@vu.nl>
2015-07-30 13:01:56 +0200

    -- naf3.0

--- newsreader-vm/vua-eventcoreference/scripts/event-coref-naf-file.sh
+++
@@ -1,3 +0,0 @@
-#### Within document event coreference baseline (lemma matches)
-java -Xmx812m -cp ../lib/EventCoreference-1.0-SNAPSHOT-jar-with-dependencies.jar eu.newsreader.eventcoreference.naf.EventCorefLemmaBaseline --naf-file "../example/enPM.naf" > "../example/enPM.coref.naf"
-#java -Xmx812m -cp "../lib/EventCoreference-1.0-SNAPSHOT.jar":"../lib/KyotoKafSaxParser-1.0-jar-with-dependencies.jar" eu.newsreader.eventcoreference.naf.EventCorefLemmaBaseline --naf-file "../example/enPM.naf" > "../example/enPM.coref.naf"
