b0b9ee6dd0232963c0a1f6da215445e9bb8fade9
piekvossen <piek.vossen@vu.nl>
2017-01-09 14:57:19 +0100

    -- @deprecated storyteller classes, preparing removal

--- scripts/naf2sem-grasp.sh
+++ scripts/naf2sem-grasp.sh
@@ -6,4 +6,4 @@ RESOURCES="$( cd $ROOT && cd .. && pwd)"/vua-resources
 # assumes vua-resources is installed next to this installation
 # git clone https://github.com/cltl/vua-resources.git
 
-java -Xmx2000m -cp "$LIB/EventCoreference-v3.1.2-jar-with-dependencies.jar" eu.newsreader.eventcoreference.naf.GetSemFromNafStream --non-entities --project wikinews --all --ili "$RESOURCES/ili.ttl.gz" --perspective --eurovoc-en "$RESOURCES//mapping_eurovoc_skos.csv.gz" --source-frames "$RESOURCES/source.txt"
+java -Xmx2000m -cp "$LIB/EventCoreference-v3.1.2-jar-with-dependencies.jar" eu.newsreader.eventcoreference.naf.GetSemFromNafStream --non-entities --project wikinews --all --ili "$RESOURCES/ili.ttl.gz" --perspective --eurovoc-en "$RESOURCES/mapping_eurovoc_skos.label.concept.gz" --source-frames "$RESOURCES/source.txt"
