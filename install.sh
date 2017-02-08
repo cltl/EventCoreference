ca63493dd1c20dcf1e1efa31fe2a60cb76d93727
Paul Huygen <paul.huygen@huygen.nl>
2017-02-08 14:55:15 +0100

    Make install.sh more general

--- install.sh
+++ install.sh
@@ -6,15 +6,30 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PARENT="$( cd $DIR && cd .. && pwd)"
 
-mkdir "$DIR/lib"
+mkdir -p "$DIR/lib"
 echo "#1. compiling the library from source code and dependencies"
 mvn clean
 mvn install
 echo "#2. moving binary to lib folder"
-mv "$DIR/target/EventCoreference-v3.0-jar-with-dependencies.jar" "$DIR/lib"
+jarfile=`find $DIR/target -name "EventCoreference-v*-jar-with-dependencies.jar" -print`
+if
+  [ "$jarfile" == "" ]
+then
+  echo "could not generate the jar file. Sorry ..."
+  exit 4
+fi
+mv $jarfile $DIR/lib
 echo "#3. installing the vua-resources"
 cd "$PARENT"
-git clone https://github.com/cltl/vua-resources.git
+if
+  [ -d "vua-resources" ]
+then
+  cd vua-resources
+  git pull
+  cd ..
+else
+  git clone https://github.com/cltl/vua-resources.git
+fi
 echo "#4. cleaning up"
 cd "$DIR"
 rm -R target
